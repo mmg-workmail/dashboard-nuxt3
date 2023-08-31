b5fa042f4f69382e544615de494b6d13a27f48b3
mmg.workmail <mmg.workmail@gmail.com>
2023-09-01 01:20:01 +0300

    add nuxt 3

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    locales: [
+      {
+        code: "fa",
+        file: "fa/index.ts",
+        name: "فا",
+        rtl: true,
+      },
+      {
+        code: "en",
+        file: "en/index.ts",
+        name: "En",
+        rtl: false,
+      },
+    ],
+    lazy: true,
+    langDir: "src/languages",
+    defaultLocale: "en",
+  },
+});
